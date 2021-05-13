f1bff817c89880bacca824eec4718ca4d7b30017
Zxynine <79198508+Zxynine@users.noreply.github.com>
2021-05-12 20:55:16 -0400

    Update TinyBME280.h

--- TinyBME280.h
+++ TinyBME280.h
@@ -1,7 +1,4 @@
-/* TinyBME280 Library v2
-
-   David Johnson-Davies - www.technoblogy.com - 22nd June 2019
-   
+/* TinyBME280 Library expanded
    CC BY 4.0
    Licensed under a Creative Commons Attribution 4.0 International license: 
    http://creativecommons.org/licenses/by/4.0/
